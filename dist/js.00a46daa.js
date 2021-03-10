77ccaae7809ff4d744d2daa2ba93ce696da4a3de
Matt M Moncada <misakamikotocsr@gmail.com>
2021-03-10 13:44:35 -0700

    fixed navigation hamburger

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -121,10 +121,16 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 // import '../scss/main.scss'
 // import '../css/main.css'
 var home = document.querySelector('.home');
+var contact = document.querySelector('.contact');
+var resume = document.querySelector('.resume');
+var portfolio = document.querySelector('.portfolio');
 var menuIcon = document.querySelector('.menu-icon');
 var bars = document.querySelector('.fa-bars');
 var times = document.querySelector('.fa-times');
 menuIcon.addEventListener("click", toggleMenu);
+menuIcon.addEventListener("click", toggleMenuContact);
+menuIcon.addEventListener("click", toggleMenuResume);
+menuIcon.addEventListener("click", toggleMenuPortfolio);
 
 function toggleMenu() {
   if (home.classList.contains("menu-active")) {
@@ -133,6 +139,42 @@ function toggleMenu() {
     bars.classList.add("fa-bars");
   } else {
     home.classList.add("menu-active");
+    bars.classList.add("fa-times");
+    bars.classList.remove("fa-bars");
+  }
+}
+
+function toggleMenuContact() {
+  if (contact.classList.contains("menu-active")) {
+    contact.classList.remove("menu-active");
+    bars.classList.remove("fa-times");
+    bars.classList.add("fa-bars");
+  } else {
+    contact.classList.add("menu-active");
+    bars.classList.add("fa-times");
+    bars.classList.remove("fa-bars");
+  }
+}
+
+function toggleMenuResume() {
+  if (resume.classList.contains("menu-active")) {
+    resume.classList.remove("menu-active");
+    bars.classList.remove("fa-times");
+    bars.classList.add("fa-bars");
+  } else {
+    resume.classList.add("menu-active");
+    bars.classList.add("fa-times");
+    bars.classList.remove("fa-bars");
+  }
+}
+
+function toggleMenuPortfolio() {
+  if (portfolio.classList.contains("menu-active")) {
+    portfolio.classList.remove("menu-active");
+    bars.classList.remove("fa-times");
+    bars.classList.add("fa-bars");
+  } else {
+    portfolio.classList.add("menu-active");
     bars.classList.add("fa-times");
     bars.classList.remove("fa-bars");
   }
@@ -165,7 +207,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50896" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51124" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,17 @@
 // import '../css/main.css'
 
 var home = document.querySelector('.home');
+var contact = document.querySelector('.contact');
+var resume = document.querySelector('.resume');
+var portfolio = document.querySelector('.portfolio');
 var menuIcon = document.querySelector('.menu-icon');
 var bars = document.querySelector('.fa-bars');
 var times = document.querySelector('.fa-times');
 
 menuIcon.addEventListener("click", toggleMenu);
+menuIcon.addEventListener("click", toggleMenuContact);
+menuIcon.addEventListener("click", toggleMenuResume);
+menuIcon.addEventListener("click", toggleMenuPortfolio);
 
 function toggleMenu(){
   if (home.classList.contains("menu-active")) {
@@ -16,6 +22,45 @@ function toggleMenu(){
 
   } else {
     home.classList.add("menu-active");
+    bars.classList.add("fa-times");
+    bars.classList.remove("fa-bars");
+
+  }
+}
+function toggleMenuContact(){
+  if (contact.classList.contains("menu-active")) {
+    contact.classList.remove("menu-active");
+    bars.classList.remove("fa-times");
+    bars.classList.add("fa-bars");
+
+  } else {
+    contact.classList.add("menu-active");
+    bars.classList.add("fa-times");
+    bars.classList.remove("fa-bars");
+
+  }
+}
+function toggleMenuResume(){
+  if (resume.classList.contains("menu-active")) {
+    resume.classList.remove("menu-active");
+    bars.classList.remove("fa-times");
+    bars.classList.add("fa-bars");
+
+  } else {
+    resume.classList.add("menu-active");
+    bars.classList.add("fa-times");
+    bars.classList.remove("fa-bars");
+
+  }
+}
+function toggleMenuPortfolio(){
+  if (portfolio.classList.contains("menu-active")) {
+    portfolio.classList.remove("menu-active");
+    bars.classList.remove("fa-times");
+    bars.classList.add("fa-bars");
+
+  } else {
+    portfolio.classList.add("menu-active");
     bars.classList.add("fa-times");
     bars.classList.remove("fa-bars");
 
